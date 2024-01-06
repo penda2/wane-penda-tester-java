f71cc267704d662306b83e621b3b24cd7a531978
Penda Wane <pendawane@yahoo.fr>
2024-01-06 11:23:31 +0100

    Implementation de la fonctionnalité des 30min gratuites / FareCalculatorService

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -4,23 +4,24 @@ import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
-
+ 
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
-
+      
         long inHour = ticket.getInTime().getTime();
         long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
         long millisDuration = outHour - inHour;
         float duration = (millisDuration / 3600000) + (millisDuration % 3600000 / 60000 / 60.f);
-        
-
-        switch (ticket.getParkingSpot().getParkingType()){ 
+        if (duration <= 0.50) {
+        	ticket.setPrice(0);
+		}
+        else {
+            switch (ticket.getParkingSpot().getParkingType()){ 
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR );
                 break;
             }
             case BIKE: {
@@ -29,5 +30,8 @@ public class FareCalculatorService {
             } 
             default: throw new IllegalArgumentException("Unkown Parking Type");
         }
+		}
+
+
     }
 }
